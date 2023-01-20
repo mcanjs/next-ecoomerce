e4610936f048efb1c1d28e51ed04a7b2d44a705c
mcanjs <mcanspin@gmail.com>
2023-01-20 16:45:02 +0300

    husky command rule changed

--- server/src/services/search.service.ts
+++ server/src/services/search.service.ts
@@ -1,17 +1,17 @@
-import { Category } from '@/interfaces/category.interface';
-import { Product } from '@/interfaces/products.interface';
+// import { Category } from '@/interfaces/category.interface';
+// import { Product } from '@/interfaces/products.interface';
 import categoryModel from '@/models/category.model';
 import productModel from '@/models/products.model';
-import { escapeRegex } from '@/utils/util';
+// import { escapeRegex } from '@/utils/util';
 
 class SearchService {
   public categoryModel = categoryModel;
   public productModel = productModel;
-  public async findCategoryOrProductByKey(key: string): Promise<Product & Category> {
-    const regex = new RegExp(escapeRegex(key), 'gi');
-    const searchProductByKey: Product = await this.productModel.find({});
-    return { product: searchProductByKey };
-  }
+  // public async findCategoryOrProductByKey(key: string): Promise<Product & Category> {
+  //   const regex = new RegExp(escapeRegex(key), 'gi');
+  //   const searchProductByKey: Product = await this.productModel.find({});
+  //   return { product: searchProductByKey };
+  // }
 }
 
 export default SearchService;
